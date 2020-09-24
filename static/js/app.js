7b867d0f1967bd4ba9e174cec0a352ae38018fc1
ChristinaAS <smiley9824@aol.com>
2020-09-23 23:24:03 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -13,30 +13,45 @@ d3.json("samples.json").then((data) => {
     //console.log
     //mydata = data
 function optionChanged(metaID) {
+    metaDatas(metaID);
+    charts(metaID);
+
+}
+
+function metaDatas(metaID) {    
     d3.json("samples.json").then((data) => {
         let metadata = data.metadata;
-        let Array = metadata.filter(obj => obj.id == metaID);
-        let result = Array[0];
+        let resultArray = metadata.filter(obj => obj.id == metaID)[0];
+        //let result = Array[0];
         let metaPanel = d3.select("#sample-metadata");
         metaPanel.html("");
-        Object.entries(result).forEach((item) => {
-            metaPanel.append("h6").text(item[0]+ ": "+item[1]);
+        Object.entries(resultArray).forEach((item) => {
+            metaPanel.append("h6").text(item[0]+ " : "+item[1]);
         });
     });
 }
+function charts(metaID) {
+    d3.json("samples.json").then((data) => {
+        let samples = data.samples;
+        let resultArray = samples.filter(obj => obj.id == metaID)[0];
+        let sample_values = resultArray.sample_values;
+        let otu_ids = resultArray.otu_ids;
+        let otu_labels = resultArray.otu_labels;
+    console.log(resultArray)
 
+    let bar = d3.select("#bar");
+    bar.html("");
+    let barChart = [{
+        x: sample_values.slice(0,10).reverse(),
+        y: otu_ids.map(otuID => "OTU" + otuID).reverse(),
+        text: otu_labels.slice(0,10),
+        type: "bar",
+        orientation: "h"
+    }];
+    
+    var layout = {
+            title: "Top 10 Belly Button Bacteria"
+    };
 
-
-
-    // mydata.metadata.filter( )
-//    mydata.samples.filter()
-
-//    sorttableData = plotObject.otu_ids.map(obj, index)
-//    => {
-//        return {
-//            otu_id:obj,
-//            sample_value:plotObject.sample_values
-//            [index]
-//        }
-//    })
-//}
+    Plotly.newPlot("bar", barChart, layout);
+})};
